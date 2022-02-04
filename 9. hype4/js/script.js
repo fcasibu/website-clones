42f2e0141123e5020bfe302b974654f7f0209c05
nevz <projectnevz.gmail.com>
2022-02-04 18:05:24 +0800

    Add new section

--- 9. hype4/js/script.js
+++ 9. hype4/js/script.js
@@ -2,12 +2,15 @@ const menuIcon = document.querySelector('.primary__menu-icon');
 const menu = document.querySelector('.primary__links')
 
 menuIcon.addEventListener('click', () => {
-  menu.classList.toggle('show-menu')
-  menuIcon.classList.toggle('toggle-icon')
-  window.addEventListener('dblclick', () => {
-    menu.classList.remove('show-menu')
-    menuIcon.classList.remove('toggle-icon')
-  })
+  menu.classList.toggle('show-menu');
+  menuIcon.classList.toggle('toggle-icon');
+  document.body.classList.toggle('overflow')
+})
+
+window.addEventListener('dblclick', () => {
+  menu.classList.remove('show-menu');
+  menuIcon.classList.remove('toggle-icon');
+  document.body.classList.remove('overflow')
 })
 
 window.addEventListener('resize', () => {
